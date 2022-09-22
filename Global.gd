c4787a040a815b109e18e1af83c61b9f5ae910f2
tacrawl <tacrawl@iu.edu>
2022-09-22 19:45:27 -0400

    menu and HUD
    created simple HUD and MENU

--- Global.gd
+++ Global.gd
@@ -1,8 +1,54 @@
 extends Node
 
+var VP = null
+
+var score = 0
+var time = 0
+var lives = 0
+
+
 func _ready():
 	randomize()
-
+	pause_node = Node.PAUSE_MODE_PROCESS
+	VP = get_viewport().size
+	var _signal = get_tree().get_root().connect("size_changed",self," _resize")
+	reset()
+	
+	
 func _unhandled_input(event):
+	
 	if event.is_action_pressed("menu"):
+		var Pause_Menu = get_node_or_null("/root/Game/UI/Pause_Menu")
 		get_tree().quit()
+
+func _resize():
+	VP = get_viewport().size
+
+func update_score(s):
+	score += s 
+	var HUD = get_node_or_null("/root/Game/UI/HUD")
+	if Pause_Menu == null:
+		get_tree().quit()
+	else:
+		if Pause_Menu.visible:
+			Pause_Menu.hide()
+			get_tree().paused = false
+		else:
+			Pause_Menu.show()
+			get_tree().paused = true
+	if HUD != null:
+		HUD.update_score()
+	
+func update_lives(l):
+	lives += l 
+	if lives < 0: 
+		var _scene = get_tree().change_scene("res://UI/End_Game.tscn")
+	else:
+		var HUD = get_node_or_null("/root/Game/UI/HUD")
+		if HUD != null:
+			HUD.update_lives()
+	
+func reset():
+	score = 0
+	time = 30
+	lives = 5
